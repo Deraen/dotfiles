76643bdfd8a65effbc353f9002b54f944d67fec2
Juho Teperi <juho.teperi@tut.fi>
2013-07-27 21:21:05 +0300

    Awesome

---
+++ bin/parse-desktop-files.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from xdg.BaseDirectory import load_data_paths
+from xdg.DesktopEntry import DesktopEntry
+from glob import glob
+from re import sub
+
+for d in load_data_paths("applications"):
+    for app in glob(d + "/*.desktop"):
+        desktop = DesktopEntry(app)
+        if (not desktop.getHidden() and not desktop.getNoDisplay()):
+            print(desktop.getName() + "\t" + sub(r'(?i)(%U|%F)', '', desktop.getExec()))
