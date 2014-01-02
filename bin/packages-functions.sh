ba9266ffe6712cb379ab727bc507d2cd07bef429
Juho Teperi <juho.teperi@iki.fi>
2014-01-03 00:13:31 +0200

    Much better performance

--- bin/packages-functions.sh
+++ bin/packages-functions.sh
@@ -7,65 +7,78 @@ function getVersion {
         apt-cache show ${1} | grep Version | head -n1 | cut -d " " -f2
 }
 
-function mark {
-        # FIXME: This is slow? Is there some other command to get same info?
-        local auto=$(apt-mark showauto ${1} | wc -l)
-        if [[ "${auto}" == "1" ]]; then
-                echo "	Automatically installed -> mark manual"
-                apt-mark manual ${1} > /dev/null
-        fi
-}
-
 function installLocal {
-        echo "	No URL given for local package Install manually plz?"
-}
+        local package=(${1})
+        local name=${package[0]}
+        local version=${package[1]}
+        local url=${package[2]}
 
-function installRemote {
-        wget -O /tmp/${1}.deb "${3}"
-        dpkg -i /tmp/${1}.deb
+        if [[ "${url}" != "" ]]; then
+                wget -O /tmp/${name}.deb "${url}"
+                dpkg -i /tmp/${name}.deb
+        elif [[ "${version}" != "" ]]; then
+                echo "	No URL given for local package Install manually plz?"
+        fi
 }
 
 function install {
-        echo "- ${1}?"
-        INSTALL="${INSTALL}
-        ${1}"
-
-        local installed=$(dpkg --get-selections ${1} 2>&1 | grep install | wc -l)
-        if [[ "${installed}" == "0" ]]; then
-                # Second parameter = manual version
-                # Third paramter = url for manual install
-                echo "	Not installed -> installing"
-                if [[ "${3}" != "" ]]; then
-                        installRemote ${1} ${2} ${3}
-                elif [[ "${2}" != "" ]]; then
-                        installLocal ${1} ${2}
-                else
-                        apt-get install ${1}
-                fi
-        fi
-
-        if [[ "${installed}" == "1" ]] && [[ "${2}" != "" ]]; then
-                local version=$(getVersion ${1})
-                if [[ "${2}" != "${version}" ]]; then
-                        echo "	Upgrade might be available?"
-                        echo "	${version} -> ${2}"
-                        if [[ -z "${3}" ]]; then
-                                installLocal ${1} ${2}
-                        else
-                                installRemote ${1} ${2} ${3}
-                        fi
-                fi
-        fi
-
-        mark ${1}
+        INSTALL="${INSTALL}"$'\n'"${1} ${2} ${3}"
 }
 
 function markauto {
         # Check for manually installed packages which are not needed
-        for x in $(apt-mark showmanual); do
+        local manual=($(apt-mark showmanual))
+
+        OIFS=$IFS; IFS=$'\n'; INSTALL=($INSTALL); IFS=$OIFS
+        for x in "${INSTALL[@]}"; do
+                local package=(${x})
+                local name=${package[0]}
+                local version=${package[1]}
+                local url=${package[2]}
+
+                echo "- ${name}?"
+                local installed=$(dpkg --get-selections ${name} 2>&1 | grep install | wc -l)
+                if [[ "${installed}" == "0" ]]; then
+                        # Second parameter = manual version
+                        # Third paramter = url for manual install
+                        echo "	Not installed -> installing"
+                        if [[ "${version}" != "" ]]; then
+                                installLocal ${x}
+                        else
+                                apt-get install ${name}
+                        fi
+                fi
+
+                # Check version if package is already installed and version is defined (="local package")
+                if [[ "${installed}" == "1" ]] && [[ "${version}" != "" ]]; then
+                        local current_version=$(getVersion ${name})
+                        if [[ "${version}" != "${current_version}" ]]; then
+                                echo "	Upgrade might be available?"
+                                echo "	${current_version} -> ${version}"
+                                installLocal ${x}
+                        fi
+                fi
+
+                local auto="1"
+                for y in "${manual[@]}"; do
+                        if [[ ${name} == ${y} ]]; then
+                                auto="0"
+                                break
+                        fi
+                done
+
+                if [[ "${auto}" == "1" ]]; then
+                        echo "	Automatically installed -> mark manual"
+                        apt-mark manual ${name} > /dev/null
+                fi
+        done
+
+        for x in ${manual[@]}; do
                 needed="0"
-                for y in ${INSTALL}; do
-                        if [[ "${x}" == "${y}" ]]; then
+                for y in "${INSTALL[@]}"; do
+                        local package=(${x})
+                        local name=${package[0]}
+                        if [[ ${x} == ${name} ]]; then
                                 needed="1"
                                 break
                         fi
@@ -77,3 +90,4 @@ function markauto {
                 fi
         done
 }
+
