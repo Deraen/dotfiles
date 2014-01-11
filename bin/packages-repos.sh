0a82afa22e3e69808b936a059bc501cef192ce9b
Juho Teperi <juho.teperi@iki.fi>
2014-01-11 03:46:56 +0200

    Improved repo handling

--- bin/packages-repos.sh
+++ bin/packages-repos.sh
@@ -4,21 +4,44 @@
 # NOTE: Doesn't support deb-src
 
 SOURCE_FILES=""
+OPS=0
+SOURCESDIR="/etc/apt/sources.list.d"
+
+function confirm {
+        read -r -p "$1 [Y/n]" yes
+        yes=${yes,,}
+        if [[ $yes =~ ^(yes|y| ) ]]; then
+                echo 0 # true
+        else
+                echo 1 # false
+        fi
+}
+
+function save {
+        local filename=$1
+        local content="$2"
+
+        SOURCE_FILES="${SOURCE_FILES}
+        ${filename}"
+
+        local new=($(echo "$content" | md5sum | cut -f1))
+        local old=($(md5sum $SOURCESDIR/$filename))
+        if [[ $new != $old ]]; then
+                echo "PPA/repo changed or added"
+                echo $content > $SOURCESDIR/${filename}
+                OPS=$((OPS + 1))
+        fi
+}
 
 function ppa {
         local filename="${1}-$(echo ${2} | sed 's/\./_/')-${3}.list"
-        SOURCE_FILES="${SOURCE_FILES}
-        ${filename}"
+        local content="deb http://ppa.launchpad.net/${1}/${2}/ubuntu ${3} main"
 
-        echo "deb http://ppa.launchpad.net/${1}/${2}/ubuntu ${3} main" > /etc/apt/sources.list.d/${filename}
+        save $filename "$content"
 }
 
 function repo {
-        local filename="${1}.list"
-        SOURCE_FILES="${SOURCE_FILES}
-        ${filename}"
-
-        echo "${2}" > /etc/apt/sources.list.d/${filename}
+        save ${1}.list "$2"
 }
 
 function clearRepos {
@@ -42,10 +65,19 @@ function clearRepos {
         done
 
         if [[ "${extras}" != "" ]]; then
-                read -r -p "Remove extras? [Y/n] " yes
-                yes=${yes,,} # tolower
-                if [[ "${yes}" =~ ^(yes|y| ) ]]; then
+                if confirm "Remove extras"; then
                         rm ${extras}
+                        OPS=$((OPS + 1))
+                fi
+        fi
+
+        if [[ $OPS -gt 0 ]]; then
+                if confirm "PPAs/repos changed, run getkeys"; then
+                        launchpad-getkeys
+                fi
+
+                if confirm "PPAs/repos changed, run update"; then
+                        apt-get update
                 fi
         fi
 }
