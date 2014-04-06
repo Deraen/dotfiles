915b8c91bde3e60d838b1355c892e6d828f4fbe7
Juho Teperi <juho.teperi@iki.fi>
2014-04-06 19:00:14 +0300

    CLojure stuff

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,24 @@
-{:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-ancient "0.5.4"]
-                  [lein-midje "3.1.3"]]
-        :dependencies [[redl "0.2.2"]]
-        :jvm-opts ^:replace []}}
+{:user {:plugins [[lein-ancient "0.5.4"]
+                  [lein-midje "3.1.3"]
+                  ]
+        :dependencies [[redl "0.2.2"]
+                       [org.clojure/tools.namespace "0.2.4"]
+                       [io.aviso/pretty "0.1.8"]
+                       [leiningen "2.3.4"]
+                       [im.chit/vinyasa "0.2.0"]
+                       ]
+        :injections [(require 'vinyasa.inject)
+                     (vinyasa.inject/inject 'clojure.core
+                                            '[[vinyasa.inject inject]
+                                              [vinyasa.pull pull]
+                                              [vinyasa.lein lein]
+                                              [vinyasa.reimport reimport]])
+                     (vinyasa.inject/inject 'clojure.core '>
+                                            '[[cemerick.pomegranate add-classpath get-classpath resources]
+                                              [clojure.tools.namespace.repl refresh]
+                                              [clojure.repl apropos dir doc find-doc source pst [root-cause >cause]]
+                                              [clojure.pprint pprint]
+                                              [clojure.java.shell sh]])
+                     ]
+        }}
 
--- .vim/after/plugin/fireplace.vim
+++ .vim/after/plugin/fireplace.vim
@@ -1,3 +1,38 @@
 autocmd FileType clojure nunmap <buffer> K
 autocmd FileType clojure nmap <buffer> <F2>  <Plug>FireplaceK
 autocmd FileType clojure nmap <buffer> <F3>  <Plug>FireplaceSource
+
+function! s:Refresh(bang, ns)
+  let cmd = (('(>refresh)'))
+  try
+    silent! write
+    call fireplace#session_eval(cmd)
+    return ''
+  catch /^Clojure:.*/
+    return ''
+  endtry
+endfunction
+
+function! s:setup_refresh()
+  command! -buffer -bar -bang -complete=customlist,fireplace#ns_complete -nargs=? Refresh :exe s:Refresh(<bang>0, <q-args>)
+  nnoremap <silent><buffer> cpf :Refresh<CR>
+endfunction
+
+autocmd FileType clojure call s:setup_refresh()
+
+function! s:MidjeTest(bang, ns)
+  let cmd = (("(use 'midje.repl)(midje.repl/load-facts :all)"))
+  try
+    call fireplace#session_eval(cmd)
+    return ''
+  catch /^Clojure:.*/
+    return ''
+  endtry
+endfunction
+
+function! s:setup_test()
+  command! -buffer -bar -bang -complete=customlist,fireplace#ns_complete -nargs=? MidjeTest :exe s:MidjeTest(<bang>0, <q-args>)
+  nnoremap <silent><buffer> cpt :MidjeTest<CR>
+endfunction
+
+autocmd FileType clojure call s:setup_test()
