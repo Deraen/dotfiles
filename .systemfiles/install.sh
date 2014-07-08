429c1ba9b37329f6946cce611f35736de2f0b23a
Juho Teperi <juho.teperi@iki.fi>
2014-07-08 15:58:56 +0300

    lightdm config

--- .systemfiles/install.sh
+++ .systemfiles/install.sh
@@ -4,6 +4,7 @@ installLink() {
     sudo ln -is $HOME/.systemfiles$1 $1
 }
 
+installLink "/etc/lightdm/lightdm.conf"
 installLink "/etc/default/keyboard"
 installLink "/usr/share/X11/xkb/symbols/deraen"
 # Overwrite list containing all available options... couldn't find any other way...
