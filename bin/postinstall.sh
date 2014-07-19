9124e273740d686c47a4758af2364c0418ee247d
Juho Teperi <juho.teperi@iki.fi>
2014-07-19 23:46:24 +0300

    cache ycm_build, only build some stuff @ desktop

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -7,15 +7,17 @@ git submodule update --init --recursive
 
 make -C $HOME/.vim/bundle/vimproc
 
-pushd $HOME/.vim/bundle/YouCompleteMe
-./install.sh
+mkdir $HOME/tmp/ycm_build
+pushd $HOME/tmp/ycm_build
+cmake -G "Unix Makefiles" . $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+make ycm_support_libs -j5
 popd
 
 if [[ $HOSTNAME == "juho-desktop" ]] || [[ $HOSTNAME == "juho-laptop" ]]; then
-    make -C $HOME/.local/modules/ponymix
+    make -C $HOME/.local/modules/ponymix -j5
     ln -s $HOME/.local/modules/ponymix/ponymix $HOME/.local/bin
 
-    make -C $HOME/.local/modules/dunst
+    make -C $HOME/.local/modules/dunst -j5
     ln -s $HOME/.local/modules/dunst/dunst $HOME/.local/bin
 fi
 
