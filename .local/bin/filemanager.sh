9844326931a7330f039a12c9107e6094c47120c7
Juho Teperi <juho.teperi@iki.fi>
2014-07-30 23:28:46 +0300

    use git to manage symlinks

---
+++ .local/bin/filemanager.sh
@@ -0,0 +1,1 @@
+../modules/tpm-filemanagement/filemanager.sh
--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -13,14 +13,10 @@ cmake -G "Unix Makefiles" . $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 make ycm_support_libs -j5
 popd
 
-ln -s $HOME/.local/modules/tpm-filemanagement/filemanager.sh $HOME/.local/bin
-
 if [[ $HOSTNAME == "juho-desktop" ]] || [[ $HOSTNAME == "juho-laptop" ]]; then
     make -C $HOME/.local/modules/ponymix -j5
-    ln -s $HOME/.local/modules/ponymix/ponymix $HOME/.local/bin
-
     make -C $HOME/.local/modules/dunst -j5
-    ln -s $HOME/.local/modules/dunst/dunst $HOME/.local/bin
+    make -C $HOME/.local/modules/i3blocks -j5
 fi
 
 $HOME/.systemfiles/install.sh
