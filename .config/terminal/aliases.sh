90b18ca7baa5ecfe6407bfe576cd895ba53fce14
Juho Teperi <juho.teperi@iki.fi>
2014-08-07 16:59:44 +0300

    Better term command

--- .config/terminal/aliases.sh
+++ .config/terminal/aliases.sh
@@ -4,8 +4,6 @@ alias l='ls -CF'
 alias apt-add-key='apt-key adv --keyserver keyserver.ubuntu.com --recv-keys '
 alias ccze="ccze -A"
 alias o="open.sh"
-# Open new terminal in current dir
-alias term="( urxvt & ) &>/dev/null"
 
 _add_vim_plugin() {
   $(cd $HOME ; git submodule add https://github.com/${1}.git .vim/bundle/${1##*/})
