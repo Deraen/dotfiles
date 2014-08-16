0f45b84e59d399bd80a5fe2e03bc456538bbd7b0
Juho Teperi <juho.teperi@iki.fi>
2014-08-16 20:16:46 +0300

    i3: change volume using mouse wheel

--- .config/i3blocks/volume.sh
+++ .config/i3blocks/volume.sh
@@ -3,6 +3,10 @@ if [[ $BLOCK_BUTTON == "1" ]]; then
     gtk-launch gnome-sound-panel > /dev/null &
 elif [[ $BLOCK_BUTTON == "3" ]]; then
     ponymix toggle > /dev/null
+elif [[ $BLOCK_BUTTON == "4" ]]; then
+    ponymix -N increase 2
+elif [[ $BLOCK_BUTTON == "5" ]]; then
+    ponymix -N decrease 2
 fi
 
 if ponymix is-muted; then
