13f1d24d9ac34b47460f24f48650ca621ab627ac
Juho Teperi <juho.teperi@iki.fi>
2014-09-04 15:44:02 +0300

    Improved postinstall script

---
+++ .local/lib/functions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+confirm() {
+    local OPTIND
+    local prompt="[Y/n]"
+    local default="y"
+    while getopts ":i" opt; do
+        case $opt in
+            i)
+                default="n"
+                prompt="[y/N]"
+                ;;
+        esac
+    done
+
+    shift $(($OPTIND - 1))
+
+    read -r -p "$1 $prompt " ans
+    ans=${ans,,} # tolower
+    if [[ ! $ans ]]; then
+        ans=$default
+    fi
+    [[ $ans =~ ^(yes|y) ]]
+}
+
+header() {
+    echo
+    echo -e "\e[1m\e[32m>>"
+    echo -e ">> $1"
+    echo -e ">>\e[0m"
+}
--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+. $HOME/.local/lib/functions.sh
+
+header "Submodules"
+if confirm -i "Update all submodules?"; then
+    git submodule foreach git pull
+fi
+
 # Update all submodules to version defined
 # - Init any submodule which is not initialized
 # - Init/Update also submodules inside submodules
 git submodule update --init --recursive
 
+header "Build vimproc"
 make -C $HOME/.vim/bundle/vimproc
 
+header "Build YCM"
 mkdir $HOME/tmp/ycm_build
 pushd $HOME/tmp/ycm_build
 cmake -G "Unix Makefiles" . $HOME/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
@@ -14,9 +23,15 @@ make ycm_support_libs -j5
 popd
 
 if [[ $HOSTNAME == "juho-desktop" ]] || [[ $HOSTNAME == "juho-laptop" ]]; then
+    header "Build Ponymix"
     make -C $HOME/.local/modules/ponymix -j5
+    header "Build Dunst"
     make -C $HOME/.local/modules/dunst -j5
+    header "Build i3blocks"
     make -C $HOME/.local/modules/i3blocks -j5
 fi
 
-$HOME/.systemfiles/install.sh
+header "Systemfiles"
+if confirm -i "Install systemfiles?"; then
+    $HOME/.systemfiles/install.sh
+fi
