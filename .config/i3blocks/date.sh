1dad33e8a65581283bb9a03b4d0266bcd7a7bc6f
Juho Teperi <juho.teperi@iki.fi>
2014-10-13 15:48:42 +0300

    Sunrise.desktop

--- .config/i3blocks/date.sh
+++ .config/i3blocks/date.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $BLOCK_BUTTON == "1" ]]; then
-    gtk-launch chrome-mojepfklcankkmikonjlnidiooanmpbb-Default > /dev/null &
+    gtk-launch Sunrise > /dev/null &
 fi
 
 date +"%a %d %b %H:%M"
