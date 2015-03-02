f898f1ecafad92be938be9271c7c3e9c2a2e7061
Juho Teperi <juho.teperi@iki.fi>
2015-03-02 22:41:40 +0200

    Presentation vim

---
+++ bin/present.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+urxvt \
+    -bg "#ffffff" \
+    -pixmap ~/Dropbox/Programmers/logos/valkoinen_taustakuva.png \
+    -font "xft:Liberation Mono:pixelsize=24" \
+    -e vim -c "set background=light"
