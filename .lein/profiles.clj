1258c4ec5d746a1d7230ea721a3f857407a8d5ff
Juho Teperi <juho.teperi@iki.fi>
2015-04-23 11:14:08 +0300

    Cider wip

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,20 +1,19 @@
-{:user {:plugins [[lein-ancient "0.6.1"]
-                  [cider/cider-nrepl "0.8.2"]
+{:user {:plugins [[lein-ancient "0.6.7"]
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-deps-tree "0.1.2"]
                   [lein-pprint "1.1.2"]
-                  [venantius/ultra "0.3.2"]]
-        :dependencies [[redl "0.2.4"]
-                       [org.clojure/tools.namespace "0.2.8"]
-                       [im.chit/vinyasa.inject "0.3.0"]
-                       [im.chit/vinyasa.pull "0.3.0"]
+                  [venantius/ultra "0.3.3"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                       [org.clojure/tools.namespace "0.2.10"]
+                       [im.chit/vinyasa.inject "0.3.4"]
+                       [im.chit/vinyasa.pull "0.3.4"]
                        [aprint "0.1.3"]
-                       [cljfmt "0.1.7"]]
+                       [cljfmt "0.1.10"]]
         :ultra {:color-scheme :solarized_dark}
         :injections [(require '[vinyasa.inject :as inject])
-                     (require '[redl core complete])
                      (inject/in
                              ;; To . namespace
                              [vinyasa.inject inject]
                              [vinyasa.pull pull]
                              [clojure.tools.namespace.repl refresh clear]
-                             [aprint.core aprint ap])]}}
+                             [aprint.core aprint])]}}
