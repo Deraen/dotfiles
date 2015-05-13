3691633643204aee55827dda5410196e4bfa4191
Juho Teperi <juho.teperi@iki.fi>
2015-05-13 08:55:47 +0300

    .

--- .systemfiles/install.sh
+++ .systemfiles/install.sh
@@ -37,6 +37,7 @@ fi
 
 sudo update-alternatives --set dmenu /usr/bin/dmenu.xft
 sudo update-alternatives --set google-chrome /usr/bin/google-chrome-unstable
+sudo update-alternatives --set x-www-browser /usr/bin/google-chrome-unstable
 
 # sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 # sudo update-alternatives --set vi /usr/bin/nvim
