9431f643482059dbd8e9eb82df615583245b6d12
Juho Teperi <juho.teperi@iki.fi>
2015-07-01 00:21:58 +0300

    wip repository checker

--- bin/check-repositories.sh
+++ bin/check-repositories.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 myrelease=$(grep 'DISTRIB_CODENAME' /etc/lsb-release | sed 's/DISTRIB_CODENAME=//' | head -1)
-repo_list=$(cd /etc/apt/sources.list.d && cat $repo_clean /etc/apt/sources.list | grep deb\ http.* | sed -e 's/.*help\.ubuntu\.com.*//' -e 's/^#.*//' -e 's/deb\ //' -e 's/deb-src\ //' -e '/^$/d' | sort -u | awk '{print $1"|"$2}' | sed -e 's/\/|/|/' -e 's/-[a-z]*$//' | uniq && cd)
-
+# repo_list=$(cd /etc/apt/sources.list.d && cat *.list /etc/apt/sources.list | grep deb\ http.* | sed -e 's/.*help\.ubuntu\.com.*//' -e 's/^#.*//' -e 's/deb\ //' -e 's/deb-src\ //' -e '/^$/d' | sort -u | awk '{print $1"|"$2}' | sed -e 's/\/|/|/' -e 's/-[a-z]*$//' | uniq && cd)
 # releases=("vivid" "utopic" "trusty" "precise")
 
-for repo_0 in $repo_list; do
-  repo="$(echo $repo_0 | sed 's/|.*//')"
-  rir="$(echo $repo_0 | sed 's/.*|//')"
+for file in $(find /etc/apt -type f -name '*.list'); do
+  OIFS=$IFS
+  IFS=$'\n'
+  for source in $(grep "deb http.*" $file | sed -e 's/.*help\.ubuntu\.com.*//' -e 's/^#.*//' -e 's/deb\ //' -e 's/deb-src\ //' -e '/^$/d'); do
+    IFS=$' '
+    source=($source)
+    url=${source[0]}
+    series=${source[1]}
 
-  rir_list=("$(curl --silent $repo/dists/ | grep -oi href\=\"[^\/].*/\" | sed -e 's/href\=\"//i' -e 's/\/\"//' -e 's/-.*//' -e 's/\ NAME.*//i' | sort -u | uniq)")
-  if [ '$rir_list' = '' ]; then
-    rir_list=("$(curl --silent $repo/ | grep -oi href\=\"[^\/].*\" | sed -e 's/href\=\"//i' -e 's/\/\"//' -e 's/-.*//' -e 's/\ NAME.*//i' -e 's/\/index\.html\"//' -e 's/.*".*//' -e 's/http.*//' | sort -u | uniq)")
-  fi
+    IFS=$'\n'
+    avail=($(curl --silent $url/dists/ | grep -oi href\=\"[^\/].*/\" | sed -e 's/href\=\"//i' -e 's/\/\"//' -e 's/-.*//' -e 's/\ NAME.*//i' | sort -u | uniq))
 
-  echo "$repo $rir_list"
+    IFS=$' '
+    avail=(${avail[@]//\.\./})
+
+    avail_f=$(IFS=, ; echo "${avail[*]}")
+    echo "${file} - ${url} current: ${series}; available: ${avail_f}"
+  done
+  IFS=$OIFS
 done
