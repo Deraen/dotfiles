5ceac38a861f86915fc3c5ad1be13ed5b31aabaf
Juho Teperi <juho.teperi@iki.fi>
2015-07-12 12:53:27 +0300

    Testing parallel submodule update

---
+++ bin/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PARALLEL_MAX=$(($(nproc) * 2))
+
+update_module() {
+  while [ $(ps --no-headers -o pid --ppid=$$ | wc -l) -gt $PARALLEL_MAX ]; do
+    sleep .1
+  done
+  cd "$@" && git checkout && git pull origin master && git add "$@" && echo "<<< Ready $@" &
+}
+
+submodules=($(git submodule foreach --quiet pwd))
+
+for m in ${submodules[@]}; do
+  echo ">>> Update $m"
+  update_module "$m"
+done
+
+wait
+
+git submodule update --init --recursive
