77d7ec04ca9312e048c871576661f117c7b31abc
Juho Teperi <juho.teperi@iki.fi>
2015-12-08 22:43:49 +0000

    Separate lein repl profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[lein-ancient "0.6.7"]
-                  [cider/cider-nrepl "0.9.1"]
+{:repl {:plugins [[cider/cider-nrepl "0.9.1"]
                   [refactor-nrepl "1.1.0"]
+                  [venantius/ultra "0.3.3"]]}
+ :user {:plugins [[lein-ancient "0.6.7"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-pprint "1.1.2"]
-                  [venantius/ultra "0.3.3"]]
+                  [lein-pprint "1.1.2"]]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [im.chit/vinyasa.inject "0.3.4"]
