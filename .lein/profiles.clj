183f0d853b8e224a2c06103e2d4f6f64692cd3c3
Juho Teperi <juho.teperi@iki.fi>
2015-12-11 09:53:42 +0200

    Fix lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,7 @@
                   [venantius/ultra "0.3.3"]]}
  :user {:plugins [[lein-ancient "0.6.7"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-pprint "1.1.2"]]]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [im.chit/vinyasa.inject "0.3.4"]
