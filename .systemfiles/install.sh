f47325c44a24a491dfb9e724fbb3a03736d2f236
Juho Teperi <juho.teperi@iki.fi>
2016-01-31 16:22:26 +0200

    Use nvim

--- .systemfiles/install.sh
+++ .systemfiles/install.sh
@@ -35,9 +35,9 @@ fi
 sudo update-alternatives --set google-chrome /usr/bin/google-chrome-beta
 sudo update-alternatives --set x-www-browser /usr/bin/google-chrome-beta
 
-# sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-# sudo update-alternatives --set vi /usr/bin/nvim
-# sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-# sudo update-alternatives --set vim /usr/bin/nvim
-# sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-# sudo update-alternatives --set editor /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --set vi /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --set vim /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --set editor /usr/bin/nvim
