e849358cd8f4330f82ed4a1f2c39156e0fcc5424
Juho Teperi <juho.teperi@iki.fi>
2016-03-23 09:42:58 +0200

    lein-licenses

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,8 @@
                   [venantius/ultra "0.3.3"]]}
  :user {:plugins [[lein-ancient "0.6.7"]
                   [lein-deps-tree "0.1.2"]
-                  [lein-pprint "1.1.2"]]
+                  [lein-pprint "1.1.2"]
+                  [lein-licenses "0.2.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [aprint "0.1.3"]]
