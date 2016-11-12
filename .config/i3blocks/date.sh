4cfb0f4d73e5cbec01db3b2bc41822e839021a90
Juho Teperi <juho.teperi@iki.fi>
2016-11-11 19:53:45 -0800

    Add helsinki time to panel when abroad

--- .config/i3blocks/date.sh
+++ .config/i3blocks/date.sh
@@ -3,6 +3,13 @@ if [[ $BLOCK_BUTTON == "1" ]]; then
     scratchpad.py "gtk-launch Sunrise" "crx_mojepfklcankkmikonjlnidiooanmpbb" > /dev/null &
 fi
 
-date +"%a %d %b %H:%M"
-date +"%a %d %b %H:%M"
+now=$(date +"%a %d %b %H:%M")
+tz=$(date +"%Z")
+hel=""
+if [[ $tz != "EET" ]] && [[ $tz != "EEST" ]]; then
+    hel=" | Hel: $(TZ="Europe/Helsinki" date +"%a %d %H:%M")"
+fi
+
+echo $now$hel
+echo $now$hel
 echo "#f3759a"
