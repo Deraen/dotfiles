7aec24a9c965e6138c05a15cdbf755697fd68898
Juho Teperi <juho.teperi@iki.fi>
2017-02-05 23:48:55 +0200

    Update default Boot version and cider-nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
-{:repl {:plugins [[cider/cider-nrepl "0.13.0"]
+{:repl {:plugins [[cider/cider-nrepl "0.14.0"]
                   [refactor-nrepl "2.2.0"]
-                  [venantius/ultra "0.5.0" :exclusions [org.clojure/clojure]]]}
+                  [venantius/ultra "0.5.1"]]}
  :user {:plugins [[lein-ancient "0.6.10"]
                   [lein-deps-tree "0.1.2"]
                   [lein-pprint "1.1.2"]
-                  [lein-licenses "0.2.0"]]
+                  [lein-licenses "0.2.1"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [aprint "0.1.3"]]
