89c2709bfced4a036d7bd37386368a0eb2181b09
Juho Teperi <juho.teperi@iki.fi>
2017-03-19 18:20:28 +0200

    Add colors for light terminal theme

--- bin/present.sh
+++ bin/present.sh
@@ -7,8 +7,27 @@ if [[ ! -z $1 ]]; then
 fi
 
 exec urxvt \
-    -bg "#ffffff" \
-    -fg "#222222" \
-    -pixmap "$HOME/Dropbox (Metosin)/Metosin Team Folder/logos/valkoinen_taustakuva.png" \
+    --background "#ffffff" \
+    --foreground "#222222" \
+    --color0 "#4e4e4e" \
+    --color1 "#AE6D6D" \
+    --color2 "#517351" \
+    --color3 "#8C640B" \
+    --color4 "#7092B3" \
+    --color5 "#A68787" \
+    --color6 "#769999" \
+    --color7 "#9C9C9C" \
+    --color8 "#626262" \
+    --color9 "#BF5478" \
+    --color10 "#769976" \
+    --color11 "#BF862F" \
+    --color12 "#95B7D9" \
+    --color13 "#DE9898" \
+    --color14 "#78BFBF" \
+    --color15 "#ABABAB" \
+    --cursorColor "#5C5C5C" \
+    --cursorColor2 "#3a3a3a" \
+    --colorBD "#333333" \
+    # -pixmap "$HOME/Dropbox (Metosin)/Metosin Team Folder/logos/valkoinen_taustakuva.png" \
     -font "xft:Liberation Mono:pixelsize=24" \
     -e $0 -x
