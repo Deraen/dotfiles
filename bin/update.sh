7edbe59aa9d0c408e0f4b7a14f0ed1cb91a14eb1
Juho Teperi <juho.teperi@iki.fi>
2017-08-05 03:09:13 +0300

    Update deps

--- bin/update.sh
+++ bin/update.sh
@@ -5,4 +5,4 @@ git submodule update --remote
 ( cd "$NVM_DIR" && git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" origin) )
 ( cd $HOME/.local/modules/rofi && git checkout $(git describe --abbrev=0 --tags --match "[0-9]*" origin) )
 
-git add -A $HOME/.local/modules $HOME/.vim/bundle
+git add -A $HOME/.local/modules $HOME/.vim/bundle*
