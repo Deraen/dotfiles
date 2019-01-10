412d61537d6590f02ea6461f30a1d9470b36f0c2
Juho Teperi <juho.teperi@iki.fi>
2019-01-10 14:25:57 +0200

    Update lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,12 +1,12 @@
 {:repl {:plugins [[cider/cider-nrepl "0.18.0"]
                   [refactor-nrepl "2.4.0"]
-                  [venantius/ultra "0.5.2"]]}
+                  #_[venantius/ultra "0.5.2"]]}
  :user {:plugins [[lein-ancient "0.6.15"]
-                  [lein-deps-tree "0.1.2"]
+                  [lein-deps-tree "0.1.2" :exclusions [org.clojure/clojure]]
                   [lein-pprint "1.2.0"]
                   [lein-licenses "0.2.2"]
                   [lein-try "0.4.3"]]
-        :dependencies [[nrepl "0.4.4"]
+        :dependencies [[nrepl "0.5.3"]
                        [org.clojure/tools.namespace "0.2.11"]
                        [aprint "0.1.3"]]
         :ultra {:color-scheme :solarized_dark}}
