f32a8cfbea1d3ad7343e405d75a6ef4ff9f1223a
Juho Teperi <juho.teperi@iki.fi>
2020-01-21 23:54:05 +0200

    Use wallpaper for lockscreen

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-i3lock -c "#1b1b1b" -e
+mkdir -p "/home/$USER/.cache/lock_wallpapers"
+
+read -r IMAGE COLOR < <(rndbg.sh)
+
+INAME=$(basename "$IMAGE")
+PNG=/home/$USER/.cache/lock_wallpapers/$INAME.png
+
+if [[ ! -f "$PNG" ]]; then
+    convert "$IMAGE" -resize 3840x2160 -gravity center -background "$COLOR" -extent 3840x2160 "$PNG"
+fi
+
+i3lock -c "#1b1b1b" -i "$PNG"
--- bin/rndbg.sh
+++ bin/rndbg.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-feh --bg-fill "$(find /home/$USER/Dropbox/Wallpapers/ -type f | sort -R | tail -1)"
+IMAGE=$(find /home/$USER/Dropbox/Wallpapers/ -type f | sort -R | tail -1)
+COLOR=$(convert "$IMAGE" -resize 1x1 -crop 100x100+0+0 +repage txt:- | grep -Po "#[[:xdigit:]]{6}")
+
+feh --bg-max "$IMAGE" --image-bg "$COLOR"
+
+echo "$IMAGE $COLOR"
