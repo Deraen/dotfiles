becb902b61f08d8e4efc0c3bcaefe7ab651120e1
Juho Teperi <juho.teperi@iki.fi>
2020-01-25 18:44:10 +0200

    Foo

--- bin/rndbg.sh
+++ bin/rndbg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-IMAGE=$(find /home/$USER/Dropbox/Wallpapers/ -type f | sort -R | tail -1)
+IMAGE=$(find "/home/$USER/Dropbox/Wallpapers/" -type f | sort -R | tail -1)
 COLOR=$(convert "$IMAGE" -resize 1x1 -crop 100x100+0+0 +repage txt:- | grep -Po "#[[:xdigit:]]{6}")
 
 feh --bg-max "$IMAGE" --image-bg "$COLOR"
