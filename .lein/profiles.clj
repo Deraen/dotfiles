3d43ba2f614693724529416337ead7a16b88b4b7
Juho Teperi <juho.teperi@iki.fi>
2021-03-13 12:25:19 +0200

    Updates

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:repl {:plugins [[cider/cider-nrepl "0.25.3"]
-                  [refactor-nrepl "2.5.0"]]}
- :user {:plugins [[lein-ancient "0.6.15"]
+{:repl {:plugins [[cider/cider-nrepl "0.25.9"]
+                  [refactor-nrepl "2.5.1"]]}
+ :user {:plugins [[lein-ancient "0.7.0"]
                   [lein-pprint "1.3.2"]
                   [lein-licenses "0.2.2"]]
         :dependencies []}
