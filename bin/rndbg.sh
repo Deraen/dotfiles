128b4503f52724a800953bed1791e56fe17a13f9
Juho Teperi <juho.teperi@iki.fi>
2021-11-22 22:53:32 +0200

    Tuning

--- bin/rndbg.sh
+++ bin/rndbg.sh
@@ -3,7 +3,7 @@ IMAGE=$(find "/home/$USER/Dropbox/Wallpapers/" -type f | sort -R | tail -1)
 COLOR=$(convert "$IMAGE" -resize 1x1 -crop 100x100+0+0 +repage txt:- | grep -Po "#[[:xdigit:]]{6}")
 
 if [[ -n $SWAYSOCK ]]; then
-    swaymsg output "*" background "$IMAGE" fit "$COLOR"
+    swaymsg output "*" background "$IMAGE" fill "$COLOR"
 else
     feh --bg-max "$IMAGE" --image-bg "$COLOR"
 fi
