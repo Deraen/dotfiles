467ce9a7a1b6f73988e9c76e083e72a92d421c5f
Juho Teperi <juho.teperi@iki.fi>
2023-09-05 15:32:34 +0300

    Fix swayidle lock on suspend

--- bin/lock.sh
+++ bin/lock.sh
@@ -6,7 +6,7 @@ if [[ $1 == "--start" ]]; then
             timeout 1800 'swaymsg "output * dpms off"' \
             resume 'swaymsg "output * dpms on"'
     else
-        swayidle -w timeout 300 'lock.sh -f' before-sleep 'lock.sh -f'
+        swayidle -w timeout 300 "${BASH_SOURCE[0]} -f" before-sleep "${BASH_SOURCE[0]} -f"
     fi
 elif [[ -n $SWAYSOCK ]]; then
     swaylock -c "#1b1b1b" "$@"
