b12fc1e9b1797cd621171cea43f42752ca95cc4a
Juho Teperi <juho.teperi@iki.fi>
2025-03-07 10:38:34 +0200

    Screenrecording colors

--- bin/record.sh
+++ bin/record.sh
@@ -8,8 +8,12 @@ name=$(xdg-user-dir VIDEOS)/$(date +'Recording_%s.mp4')
 
 # Keep proper colors
 # https://trac.ffmpeg.org/wiki/Capture/Desktop#LosslessRecording
+# Looks like Slack doesn't like the rgb colors (everything is pink)
+# Needs high10 or such h264 profile for 10bit colors
+# high10 doesn't support lossless
+# -c libx264rgb -p crf=0 -p preset=ultrafast -p color_range=2 -p format=gbrp \
 wf-recorder \
-  -c libx264rgb -p crf=0 -p preset=ultrafast \
+  -p pix_fmt=yuv422p10le -p crf=0 -p preset=ultrafast -p profile=high444 \
   -g "$(slurp)" -f "$name"
 
 echo "$name"
