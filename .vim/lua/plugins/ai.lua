c8ac6a3416981acc726c3ae61a27f9c016c57a20
Juho Teperi <juho.teperi@iki.fi>
2025-07-14 16:04:45 +0300

    .

--- .vim/lua/plugins/ai.lua
+++ .vim/lua/plugins/ai.lua
@@ -2,10 +2,18 @@ return {
   {
     -- Make sure to set this up properly if you have lazy=true
     'MeanderingProgrammer/render-markdown.nvim',
+    ft = {
+      "markdown",
+      "Avante", "codecompanion"
+    },
     opts = {
       file_types = { "markdown", "Avante", "codecompanion" },
-    },
-    ft = { "markdown", "Avante", "codecompanion" },
+      render_modes = { 'n', 'c', 't' },
+      bullet = {
+        icons = { '●', '○', '◆', '◇' },
+        right_pad = 1
+      }
+    }
   },
 
   {
